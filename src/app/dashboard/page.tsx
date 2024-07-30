a3e9194119a0ee427b7305074842be76ac9021af
unknown <vitormarinheiro9@gmail.com>
2024-07-29 21:46:53 -0300

    feat: adicionando tabela

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { Container } from "@/components/container";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../lib/auth";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { TicketItem } from "./components/ticket";
 
 export default async function Dashboard() {
     const session = await getServerSession(authOptions)
@@ -11,6 +13,32 @@ export default async function Dashboard() {
     }
 
     return (
-        <Container>Página Dashboard</Container>
+        <Container>
+            <main className="mt-9 mb-2">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-3xl font-bold">Chamados</h1>
+                    <Link href="/dashboard/new" className="bg-blue-500 px-4 py-1 rounded text-white">
+                        Abrir chamado
+                    </Link>
+                </div>
+
+                <table className="min-w-full my-2">
+                    <thead>
+                        <tr>
+                            <th className="text-medium text-left pl-1">CLIENTE</th>
+                            {/* hidden esconde o td de data e o sm:block faz com que exiba em telas maiores */}
+                            <th className="text-medium text-left hidden sm:block">DATA CADASTRO</th>
+                            <th className="text-medium text-left">STATUS</th>
+                            <th className="text-medium text-left">#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <TicketItem cliente="Vitor" data="01/02/2022" />
+                        <TicketItem cliente="Vitor" data="01/02/2022" />
+                        <TicketItem cliente="Vitor" data="01/02/2022" />
+                    </tbody>
+                </table>
+            </main>
+        </Container>
     )
 }
