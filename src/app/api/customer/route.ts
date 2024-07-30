e50278f691e4ce25f2d31073817cbc1c3c364e93
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-07-30 09:33:08 -0300

    feat: api customer

---
+++ src/app/api/customer/route.ts
@@ -0,0 +1,41 @@
+import { authOptions } from "@/app/lib/auth";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import prismaClient from '@/app/lib/prisma'
+
+
+export async function POST(request: Request) {
+
+    const session = await getServerSession(authOptions)
+
+    {/* STATUS 401 -> NÃO AUTORIZADO
+        STATUS 400 -> BAD REQUEST / REQUISIÇÃO FALHOU
+    */}
+    if (!session || !session.user) {
+        return NextResponse.json({ error: "Not authorized" }, { status: 401 })
+    }
+
+    const { name, email, phone, address, userId } = await request.json()
+
+    {/* Se tiver endereço eu mando endereço, se não envio uma string vazia */}
+
+    try {
+        await prismaClient.customer.create({
+            data: {
+                name,
+                phone,
+                email,
+                address: address ? address : "",
+                userId: userId
+            }
+        })
+
+        return NextResponse.json({ message: "Cliente cadastrado com sucesso!" })
+    } catch (err) {
+        return NextResponse.json({ error: "Failed create new customer" }, { status: 400 })
+    }
+
+
+
+    
+}
